b5d14d403a989d50d21ccc2cdb466cd66ec92ed1
bnkrueger <102695050+bnkrueger@users.noreply.github.com>
2023-11-19 18:37:41 -0600

    Add files via upload

---
+++ random_sentence_generator.py
@@ -0,0 +1,24 @@
+import requests
+
+def generate_random_sentence():
+    # use API to generate random sentence
+
+    response = requests.get("https://api.quotable.io/random")
+    data = response.json()
+
+    sentence = data["content"]
+
+    return sentence
+
+def write_to_file(sentence, filename="randomsentence.txt"):
+    # write random sentence to file
+    with open(filename, "w") as file:
+        file.write(sentence)
+
+def read_from_file(filename="randomsentence.txt"):
+    # read random sentence from file
+    with open(filename, "r") as file:
+        sentence = file.read()
+
+        return sentence
+
